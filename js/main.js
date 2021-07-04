27c037577797beca2dd50d801849295a5dafc746
Rinakae <simi1993@mail.ru>
2021-07-04 23:37:51 +0300

    create: bookmark

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   $('.comments__button').click(function(){
     $('.item-comment:nth-child(n+4):nth-child(-n+6)').css({'display' : 'block'});
   });
+
+  $('.news-block__bookmark').click(function(){
+    $(this).toggleClass('news-block__bookmark-mb');
+  });
+
+  $('.hero__bookmark').click(function(){
+    $(this).toggleClass('hero__bookmark-mb');
+  });
 });
