62eead90d083d8a2c4ab9051519a4f7376069f1b
Rinakae <simi1993@mail.ru>
2021-07-05 23:38:00 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -20,6 +20,32 @@ $(document).ready(function() {
     });
   });
 
+ 
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+  
+  document.addEventListener("keydown", function(event) {
+   if (event.key == "Escape") {
+     closeModal(event);
+   }
+  });
 
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");    
+    
+  }
 
 });
